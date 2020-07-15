11bf7b3bfe2dc47efdf8b7a137c82699a837f240
Aamir Shaikh <aamirsaras@gmail.com>
2020-07-15 14:48:28 +0000

    Added scanning.sh file

---
+++ scanning.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+echo "scanning in progress..."
+
+sleep 5
+
+echo "Scanning Successull..."
+
+echo "Please visit below link"
+
+echo "http://18.217.107.177:8080/job/maven-test/configure"

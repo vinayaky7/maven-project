16f7d7b7e86c3a880a1674e5da253f881f821825
Your Name <you@example.com>
2020-07-27 17:33:37 +0000

    updated docker.sh

--- docker.sh
+++ docker.sh
@@ -3,3 +3,4 @@
 x=$RANDOM
 y=$RANDOM
 docker run -itd --name webserver$x -p $y:80 -v my_second_volume:/var/www/html aamirs/webserver_final_version:v1.0
+elinks http://18.191.141.223:$y/webapp/target/index.jsp

8b1e24132b74f4e8b2155d3429d46f425de92357
Aamir <aamirsaras@gmail.com>
2022-07-10 01:08:19 +0530

    added eks terraform files

--- terraform/aws/resources/eks/eks-cluster.tf
+++ terraform/aws/resources/eks/eks-cluster.tf
@@ -3,7 +3,9 @@ resource "aws_eks_cluster" "radical-myeks" {
  role_arn = aws_iam_role.eks-iam-role.arn
 
  vpc_config {
-  subnet_ids = [var.subnet_id_1, var.subnet_id_2, var.subnet_id_3]
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    subnet_ids = [var.subnet_id_1, var.subnet_id_2, var.subnet_id_3]
  }
 
  depends_on = [

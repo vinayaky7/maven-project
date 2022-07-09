643a6ce8fc8e0a200d88dca198021cc0e3ba561a
Aamir <aamirsaras@gmail.com>
2022-07-10 00:56:24 +0530

    added eks terraform files

--- terraform/aws/resources/eks/eks-iam-role.tf
+++ terraform/aws/resources/eks/eks-iam-role.tf
@@ -17,14 +17,4 @@ resource "aws_iam_role" "eks-iam-role" {
  ]
 }
 EOF
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
- policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
- role    = aws_iam_role.eks-iam-role.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
- policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
- role    = aws_iam_role.eks-iam-role.name
-}
-
 }

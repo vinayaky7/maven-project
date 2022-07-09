c9db03fc0195b8c61ad2d63520d1b7b3b115e238
Aamir <aamirsaras@gmail.com>
2022-07-10 00:58:54 +0530

    added eks terraform files

--- terraform/aws/resources/eks/eks-cluster.tf
+++ terraform/aws/resources/eks/eks-cluster.tf
@@ -1,5 +1,5 @@
-resource "aws_eks_cluster" "myeks" {
- name = "myeks"
+resource "aws_eks_cluster" "radical-myeks" {
+ name = "radical-myeks"
  role_arn = aws_iam_role.eks-iam-role.arn
 
  vpc_config {

0f04493e6fe76a1fd0225f172e0a4ed88a73727b
Aamir <aamirsaras@gmail.com>
2022-07-10 01:56:50 +0530

    added eks terraform files

--- terraform/aws/resources/eks/eks-cluster.tf
+++ terraform/aws/resources/eks/eks-cluster.tf
@@ -8,6 +8,16 @@ resource "aws_eks_cluster" "radical-myeks" {
     subnet_ids = [var.subnet_id_1, var.subnet_id_2, var.subnet_id_3]
  }
 
+ resource "aws_security_group_rule" "example" {
+      type              = "ingress"
+      from_port         = 0
+      to_port           = 65535
+      protocol          = "tcp"
+      cidr_blocks       = ["0.0.0.0/0"]
+    
+      security_group_id = vpc_config.cluster_security_group_id
+ }
+
  depends_on = [
   aws_iam_role.eks-iam-role,
  ]

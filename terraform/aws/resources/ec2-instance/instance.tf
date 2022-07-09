2319eb6195747ba9de7be9dcab068487c3565350
Aamir <aamirsaras@gmail.com>
2022-07-09 17:46:00 +0530

    updated

--- terraform/aws/resources/ec2-instance/instance.tf
+++ terraform/aws/resources/ec2-instance/instance.tf
@@ -1,18 +1,9 @@
-resource "aws_vpc" "myvpc" {
-  cidr_block = "192.168.0.0/16"
-}
-
-resource "aws_subnet" "mysubnet-public-1" {
-    vpc_id = "vpc-09ea0248cd881161b"
-    cidr_block = "192.168.1.0/24"
-}
-
 resource "aws_instance" "radical-bastion" {
   ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
   instance_type = "t2.micro"
   key_name      = var.mykey
   vpc_security_group_ids = ["sg-033eb4f820faea3b3"]
-  subnet_id = "aws_subnet.mysubnet-public-1.id"
+  subnet_id = lookup(var.subnets, var.subnet, "")
   
   tags = {
     Terraform   = "true"

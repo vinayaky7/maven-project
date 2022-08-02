d41b54bec9fa8d1b6e3c4f503a21b3dcf778ad27
Aamir <aamirsaras@gmail.com>
2022-08-02 10:58:24 +0530

    updated

--- terraform/aws/resources/vpc/vars.tf
+++ terraform/aws/resources/vpc/vars.tf
@@ -1,9 +1,9 @@
 variable "AWS_ACCESS_KEY" {
-	default = "AKIA4TNG5PSZNJZYYOPX"
+	default = "AKIA2UM2VMALNDWX6RG3"
 }
 
 variable "AWS_SECRET_KEY" {
-	default = "AKIA4TNG5PSZNJZYYOPX"
+	default = "AKIA2UM2VMALNDWX6RG3"
 }
 
 variable "vpc_cidr" {
@@ -23,7 +23,7 @@ variable "private_subnet_cidr" {
 
 variable "mykey" {
 	type = string
-	default = "radical-devops-April-Ohio-12pm"
+	default = "radical-devops-weekdays-June-2022"
 	
 }
 
@@ -32,32 +32,33 @@ variable "keyPath" {
 }
 
 variable "subnet_public" {
-	default = "us-east-2a"
+	default = "us-west-2a"
 }
 
 variable "subnet_private" {
-	default = "us-east-2b"
+	default = "us-west-2b"
 }
 
 variable "subnets" {
 	type = map(string)
 	default = {
-		us-east-2a = "subnet-082e9a36b73ccb48e"
-		us-east-2b = "subnet-0c2e0b4c70b514deb"
-		us-east-2c = "subnet-03ba9aad460bd47f6"
+		us-west-2a = "subnet-058b3d997939b0f22"
+        us-west-2b = "subnet-042528506f3d17250"
+        us-west-2c = "subnet-0247e041dfb7aa47c"
+        us-west-2d = "subnet-0ac6d0d2348ff66c4"
 	}
 }
 
 
 variable "AWS_REGION" {
-  default = "us-east-2"
+  default = "us-west-2"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
     us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-0ca285d4c2cda3300"
+    us-west-2 = "ami-098e42ae54c764c35"
     eu-west-1 = "ami-0d729a60"
 	us-east-2 = "ami-0fa49cc9dc8d62c84"
   }

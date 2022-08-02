dea306729bc66abb9e454d33c5087ce21b6681c3
Aamir <aamirsaras@gmail.com>
2022-08-02 11:06:55 +0530

    updated

---
+++ terraform/aws/resources/vpc-instance-only.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "testvm" {
+  ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
+  instance_type = "t2.micro"
+  key_name      = var.mykey
+  vpc_security_group_ids = ["${aws_security_group.radical-sg.id}"]
+  subnet_id = "${aws_subnet.aws-public-subnet.id}"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Name   = "testvm"
+  }
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "/tmp/script.sh",
+    ]
+  }
+
+  # Login to the ec2-user with the aws key.
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password    = ""
+    private_key = "${file("${var.keyPath}")}"
+    host        = self.public_ip
+  }
+
+}
+

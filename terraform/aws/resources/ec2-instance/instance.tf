ed15cdb94dcd8313219c839bc891db5636f10dd0
Aamir <aamirsaras@gmail.com>
2022-10-23 15:43:02 +0530

    test

--- terraform/aws/resources/ec2-instance/instance.tf
+++ terraform/aws/resources/ec2-instance/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_network_interface" "radical_interface" {
   subnet_id   = lookup(var.subnets, var.subnet, "")
-  vpc_security_group_ids = ["sg-0180f84f7133bee9e"]
+  security_groups = ["sg-0180f84f7133bee9e"]
   private_ips = ["192.168.1.111"]
 
   tags = {

cb88382112aae32dc1472de34c77023e012fae6c
Aamir <aamirsaras@gmail.com>
2022-10-30 13:23:20 +0530

    updated

--- terraform/aws/resources/instances/ec2-instance-plain.tf
+++ terraform/aws/resources/instances/ec2-instance-plain.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "testvm" {
   //ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
   instance_type = "t2.micro"
   key_name      = var.mykey
-  vpc_security_group_ids = ["sg-033eb4f820faea3b3"]
+  vpc_security_group_ids = ["sg-071207ac18219066e"]
   subnet_id = "subnet-0f4c6ea0f186845c3"
   //subnet_id = lookup(var.subnets, var.subnet, "")
   

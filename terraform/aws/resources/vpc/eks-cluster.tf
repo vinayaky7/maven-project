0003760f15176d7bea6fd6ab704e180f7e946136
Aamir <aamirsaras@gmail.com>
2023-01-11 11:38:30 +0530

    Deploying full aws resources using terraform

--- terraform/aws/resources/vpc/eks-cluster.tf
+++ terraform/aws/resources/vpc/eks-cluster.tf
@@ -5,7 +5,7 @@ resource "aws_eks_cluster" "radical-myeks" {
  vpc_config {
     endpoint_private_access = true
     endpoint_public_access  = true
-    subnet_ids = ["${var.public_subnet_1.id}", "${var.public_subnet_2.id}", "${var.public_subnet_3.id}", "${var.public_subnet_4.id}"]
+    subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id, aws_subnet.public_subnet_4.id]
  }
 
  depends_on = [

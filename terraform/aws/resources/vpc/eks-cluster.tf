8f6de34cc4d012e231bf9ce8cccd2849d4da0801
Aamir <aamirsaras@gmail.com>
2023-01-11 12:12:41 +0530

    Deploying full aws resources using terraform

--- terraform/aws/resources/vpc/eks-cluster.tf
+++ terraform/aws/resources/vpc/eks-cluster.tf
@@ -1,5 +1,6 @@
 resource "aws_eks_cluster" "radical-myeks" {
  name = "radical-myeks"
+ version = "1.24"
  role_arn = aws_iam_role.eks-iam-role.arn
 
  vpc_config {
--- terraform/aws/resources/vpc/worker-node.tf
+++ terraform/aws/resources/vpc/worker-node.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_node_group" "radical-workernode" {
   cluster_name  = aws_eks_cluster.radical-myeks.name
   node_group_name = "radical-workernodes"
+  version         = "1.24"
   node_role_arn  = aws_iam_role.myworkernodes.arn
   subnet_ids   = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id, aws_subnet.public_subnet_4.id]
   instance_types = ["t3.medium"]

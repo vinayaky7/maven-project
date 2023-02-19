1f7e445734d844176e1649d08a98fdaa75c155af
Aamir <aamirsaras@gmail.com>
2023-02-19 11:13:20 +0530

    updated

--- terraform/aws/resources/instances/ec2-instance-plain.tf
+++ terraform/aws/resources/instances/ec2-instance-plain.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "testvm" {
-  ami           = "ami-0cea098ed2ac54925" # last parameter is the default value
+  ami           = "ami-094125af156557ca2" # last parameter is the default value
   //ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
   instance_type = "t2.micro"
   key_name      = var.mykey
-  vpc_security_group_ids = ["sg-071207ac18219066e"]
-  subnet_id = "subnet-0f4c6ea0f186845c3"
+  vpc_security_group_ids = ["sg-05e6208821489e80a"]
+  subnet_id = "subnet-07c10b20a7fb70b45"
   //subnet_id = lookup(var.subnets, var.subnet, "")
   
   tags = {
--- terraform/aws/resources/instances/vars.tf
+++ terraform/aws/resources/instances/vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_ACCESS_KEY" {
-	default = "AKIASMHI2VDZNYVXGC7X"
+	default = "AKIARDAKVDTYMPC2KPSA"
 }
 
 variable "AWS_SECRET_KEY" {
@@ -8,7 +8,7 @@ variable "AWS_SECRET_KEY" {
 
 variable "mykey" {
 	type = string
-	default = "radical-devops-weekend-Aug-2022-12pm"
+	default = "radicalnov2022weekend10am"
 	
 }
 
@@ -23,7 +23,7 @@ variable "subnet" {
 variable "subnets" {
 	type = map(string)
 	default = {
-		us-west-2a = "subnet-0f4c6ea0f186845c3"
+		    us-west-2a = "subnet-0f4c6ea0f186845c3"
         us-west-2b = "subnet-042528506f3d17250"
         us-west-2c = "subnet-0247e041dfb7aa47c"
         us-west-2d = "subnet-0ac6d0d2348ff66c4"
@@ -41,7 +41,7 @@ variable "AMIS" {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-0cea098ed2ac54925"
     eu-west-1 = "ami-0d729a60"
-	us-east-2 = "ami-0fa49cc9dc8d62c84"
+	  us-east-2 = "ami-0fa49cc9dc8d62c84"
   }
 
 }

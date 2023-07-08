a052cbcbc08e26873572f7a8d60d2b87e2bdebc4
Aamir <aamirsaras@gmail.com>
2023-07-08 11:28:30 +0530

    updated

--- terraform/aws/resources/radical-bastion/vars.tf
+++ terraform/aws/resources/radical-bastion/vars.tf
@@ -1,14 +1,14 @@
 variable "AWS_ACCESS_KEY" {
-	default = "AKIAUVDNXNRZTGLOO5FI"
+	default = "AKIASYBOLO3G5CVNDG5A"
 }
 
 variable "AWS_SECRET_KEY" {
-	default = "P052SlhYNawQ92M5R9lVg+lQVn9Q6ohFuJfCuRio"
+	default = "4QJvaCxsmKrg6CAjC+rQ4HK2vahei/eE95uU9rRT"
 }
 
 variable "mykey" {
 	type = string
-	default = "radicaljan2023-new"
+	default = "radical-april-2023-10am"
 	
 }
 
@@ -19,7 +19,7 @@ variable "subnet" {
 variable "security-group" {
 	type = map(list(string))
 	default = {
-		"mysg" = ["sg-0479eca8bb3abbab1"]
+		"mysg" = ["sg-0983b096a0db56db8"]
 		
 	}
 }
@@ -32,7 +32,7 @@ variable "keyPath" {
 variable "subnets" {
 	type = map(string)
 	default = {
-		us-west-2a = "subnet-00ce114fc73a94b73"
+		us-west-2a = "subnet-09d77de1900c2ff01"
 		us-west-2b = "subnet-02e8f731938e03cd3"
 		us-west-2c = "subnet-02be228febb9095d7"
 		us-west-2d = "subnet-098e9fded75939441"
@@ -48,7 +48,7 @@ variable "AMIS" {
   type = map(string)
   default = {
     us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-0b029b1931b347543"
+    us-west-2 = "ami-0ac64ad8517166fb1"
     eu-west-1 = "ami-0d729a60"
     us-east-2 = "ami-0fa49cc9dc8d62c84"
   }

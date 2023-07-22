047d76049cb458763e4a35a63c87145a1c8e27c4
Aamir <aamirsaras@gmail.com>
2023-07-23 01:20:29 +0530

    test

--- deploy-webservice.sh
+++ deploy-webservice.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-nohup java -jar target/dependenscy/webapp-runner.jar target/*.war  --port 9999 & >  test.log 2>&1 &
+java -jar target/dependenscy/webapp-runner.jar target/*.war  --port 9999 &

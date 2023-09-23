9dcdb7fdae67cf867fc0ffd405b3a9ab1078e860
Aamir <aamirsaras@gmail.com>
2023-09-23 13:25:53 +0530

    updated

--- terraform/aws/resources/radical-bastion/backend.tf
+++ terraform/aws/resources/radical-bastion/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {                      # Define a remote bucket (AWS S3)
     bucket = "radical-june-weekend-12pm"       # Make sure the bucket is already created in your reqgion
     key    = "terraform.tfstate"    # Set the bucket key
-    region = "us-west-2"            # Set the region where the bucket exists
+    region = "us-east-1"            # Set the region where the bucket exists
   }
   
 }
